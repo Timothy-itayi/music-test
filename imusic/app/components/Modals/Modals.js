ff8a530ad5c6f1c67089fa0618cbebb32f05c0a6
Timothy <timmytam10@gmail.com>
2024-10-24 19:58:25 +1100

    update imusic add music player to modal

--- imusic/app/components/Modals/Modals.js
+++ imusic/app/components/Modals/Modals.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Player from '../player';
 
-const Modal = ({ image, alt, isOpen, onClose }) => (
+const Modal = ({ image, alt, isOpen, onClose, soundKey }) => (
   <div className="modal" style={{ display: isOpen ? 'block' : 'none' }}>
     <div className="modal-content">
       <span className="close" onClick={onClose}>&times;</span>
       <img src={image} alt={alt} style={{ width: '100%', marginBottom: '20px' }} />
-      <h2 className="text-xl font-bold mb-2">Image Details</h2>
+
+      <Player soundKey={soundKey} />
       <p>{alt}</p>
     </div>
   </div>
